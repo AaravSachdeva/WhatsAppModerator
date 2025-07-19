88116f82283c68694cd6268cdbb69213ad7d84ae
Richie911 <oohrichmond.k@gmail.com>
2025-07-19 20:23:54 +0000

    add participant update event emitter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 // index.ts or auth.ts
-import makeWASocket, { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore } from 'baileys';
+import makeWASocket, { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore, GroupMetadata } from 'baileys';
 import QRCode from 'qrcode'
 // import P from 'pino'
 import { Boom } from '@hapi/boom'
+
+const groupId = "120363420386820501@g.us";
+
+const groupActionCheck = (action: string, participant: string) => {
+    if (action === "add") {
+        return `hello ${participant}`
+    }
+    else if (action === "remove") {
+        return `${participant} has been ${action}d`
+    }
+    else if (action === "promote") {
+        return `${participant} has been ${action}d`
+    }
+    else if (action === "demote") {
+        return `${participant} has been ${action}d`
+    }
+    return ""
+}
 
 async function startSock() {
 
@@ -29,6 +47,23 @@ async function startSock() {
                 startSock()
             }
         }
+    })
+    sock.ev.on("group-participants.update", async (request) => {
+        const { action, author, id, participants } = request;
+
+        if (id === groupId) {
+            participants.forEach(async (participant) => {
+                await sock.sendMessage(
+                    groupId,
+                    {
+                        text: groupActionCheck(action, participant),
+                        // mentions: ['12345678901@s.whatsapp.net']
+                    }
+                )
+            })
+
+        }
+
     })
 
 }
